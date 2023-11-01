47acb4e8b1842d33bd4085500d5e5c7a2c0548dc
Afdholaffanuddin07 <afdholganteng07@gmail.com>
2023-11-01 10:55:54 +0700

    js1

---
+++ Jobsheet1/lib/main.page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MyPage extends StatelessWidget {
+  const MyPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Tugas Pertama Flutter"),
+      ),
+      body: const Center(
+        child: Text("Afdhol Affanuddin 2141720253"),
+      ),
+    );
+  }
+}
